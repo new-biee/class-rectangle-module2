60e6211ef16272d65ffeae17cfb7dc6926326520
new-biee <anhvtth1907004@fpt.edu.vn>
2020-11-13 08:48:10 +0700

    Add all file

---
+++ src/com/classrectangle/Main.java
@@ -0,0 +1,20 @@
+package com.classrectangle;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the width: ");
+        double width = scanner.nextDouble();
+        System.out.print("Enter the height: ");
+        double height = scanner.nextDouble();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(width);
+        rectangle.setHeight(height);
+        System.out.println("Your Rectangle: \n" + rectangle.display());
+        System.out.println("Perimeter of the Rectangle: " + rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: " + rectangle.getArea());
+    }
+}
